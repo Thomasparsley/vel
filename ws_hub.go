838295e4fbd5034a660e7fa0ebc5f69d3ab07794
Tomáš Petržela <info@thomasparsley.cz>
2022-05-25 18:46:14 +0200

    ws hub property access

--- ws_hub.go
+++ ws_hub.go
@@ -3,24 +3,62 @@ package vel
 import "github.com/gofiber/websocket/v2"
 
 type WsHub struct {
-	clients        map[*websocket.Conn]WsConnection
-	registerChan   chan WsConnection
-	unregisterChan chan *websocket.Conn
-	broadcastChan  chan WsMessage
-	rooms          map[string]int
+	clients map[*websocket.Conn]WsClient
+	rooms   map[string]int
+
+	chanRegister   chan WsClient
+	chanBroadcast  chan WsMessage
+	chanUnregister chan *websocket.Conn
 }
 
 func NewWsHub() *WsHub {
 	return &WsHub{
-		clients:        make(map[*websocket.Conn]WsConnection),
-		registerChan:   make(chan WsConnection),
-		unregisterChan: make(chan *websocket.Conn),
-		broadcastChan:  make(chan WsMessage),
-		rooms:          make(map[string]int),
+		clients: make(map[*websocket.Conn]WsClient),
+		rooms:   make(map[string]int),
+
+		chanRegister:   make(chan WsClient),
+		chanUnregister: make(chan *websocket.Conn),
+		chanBroadcast:  make(chan WsMessage),
 	}
 }
 
-func (ws *WsHub) AddClient(c WsConnection) {
+func (ws WsHub) GetClients() map[*websocket.Conn]WsClient {
+	return ws.clients
+}
+
+func (ws WsHub) GetClient(c *websocket.Conn) WsClient {
+	return ws.clients[c]
+}
+
+func (ws WsHub) Rooms() map[string]int {
+	return ws.rooms
+}
+
+func (ws *WsHub) ChanRegister() chan WsClient {
+	return ws.chanRegister
+}
+
+func (ws *WsHub) SendRegister(c WsClient) {
+	ws.chanRegister <- c
+}
+
+func (ws *WsHub) ChanUnregister() chan *websocket.Conn {
+	return ws.chanUnregister
+}
+
+func (ws *WsHub) SendUnregister(c *websocket.Conn) {
+	ws.chanUnregister <- c
+}
+
+func (ws *WsHub) ChanBroadcast() chan WsMessage {
+	return ws.chanBroadcast
+}
+
+func (ws *WsHub) SendBroadcast(m WsMessage) {
+	ws.chanBroadcast <- m
+}
+
+func (ws *WsHub) AddClient(c WsClient) {
 	ws.clients[c.connection] = c
 	ws.rooms[c.room]++
 }
