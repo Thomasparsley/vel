08bca201d080c30665b5c63807839853f13505be
Tomáš Petržela <info@thomasparsley.cz>
2022-05-26 11:31:17 +0200

    to json bytes

--- converter/to_json.go
+++ converter/to_json.go
@@ -2,9 +2,19 @@ package converter
 
 import "encoding/json"
 
-// ToJSON convert the input to a valid JSON string.
-func ToJSON(input any) (string, error) {
+// ToJsonBytes convert the input to a valid JSON array bytes.
+func ToJsonBytes(input any) ([]byte, error) {
 	bytes, err := json.Marshal(input)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return bytes, err
+}
+
+// ToJson convert the input to a valid JSON string.
+func ToJson(input any) (string, error) {
+	bytes, err := ToJsonBytes(input)
 	if err != nil {
 		return "", err
 	}
