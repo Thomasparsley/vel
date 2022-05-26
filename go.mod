3ea87bb1c7d59b63b207fc9add582d0f21214576
Tomáš Petržela <info@thomasparsley.cz>
2022-05-26 14:34:40 +0200

    object

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/CloudyKit/jet/v6 v6.1.0 // indirect
 	github.com/bytedance/sonic v1.3.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20211229061535-45e1f0233683 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.12 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20220412001346-fc48f9fe4c15 // indirect
@@ -26,4 +28,5 @@ require (
 	github.com/valyala/fasthttp v1.37.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	gorm.io/gorm v1.23.5
 )
---
+++ object.go
@@ -0,0 +1,87 @@
+package vel
+
+import "gorm.io/gorm"
+
+type ObjectDefinition interface {
+	PK() int
+	TableName() string
+}
+
+type Object[O ObjectDefinition] struct {
+	query *gorm.DB
+}
+
+func NewObject[O ObjectDefinition](db *gorm.DB) *Object[O] {
+	var o O
+
+	return &Object[O]{
+		query: db.Table(o.TableName()),
+	}
+}
+
+// Where add conditions
+func (o *Object[O]) Where(query interface{}, args ...interface{}) *Object[O] {
+	o.query = o.query.Where(query, args...)
+	return o
+}
+
+// Joins specify Joins conditions
+func (o *Object[O]) Joins(query string, args ...interface{}) *Object[O] {
+	o.query = o.query.Joins(query, args...)
+	return o
+}
+
+// Preload preload associations with given conditions
+func (o *Object[O]) Preload(query string, args ...interface{}) *Object[O] {
+	o.query = o.query.Preload(query, args...)
+	return o
+}
+
+// First find first record that match given conditions, order by primary key
+func (o *Object[O]) First() *O {
+	var data O
+
+	o.query.First(&data)
+	if data.PK() == 0 {
+		return nil
+	}
+
+	return &data
+}
+
+// FirstOrCreate gets the first matched record or create a new one with given conditions (only works with struct, map conditions)
+func (o *Object[O]) FirstOrCreate() *O {
+	var data O
+
+	o.query.FirstOrCreate(&data)
+
+	return &data
+}
+
+// Find find records that match given conditions
+func (o *Object[O]) Find() []O {
+	var data []O
+
+	o.query.Find(&data)
+
+	return data
+}
+
+func (o *Object[O]) Exists() bool {
+	var data O
+
+	o.query.First(&data)
+	if data.PK() == 0 {
+		return false
+	}
+
+	return true
+}
+
+func (o *Object[O]) Count() int64 {
+	var data int64
+
+	o.query.Count(&data)
+
+	return data
+}
