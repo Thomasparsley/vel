46c9ef34c6b5c54b6c993ee77af977bdaabd14b3
Thomasparsley <info@thomasparsley.cz>
2022-10-10 13:51:59 +0200

    htmx update

--- vel/htmx.py
+++ vel/htmx.py
@@ -1,6 +1,10 @@
 from typing import Final
 
+from fastapi import Request
 
+
+HX_RETARGET: Final = "HX-Retarget"
+HX_RETARGET_MAIN: Final = {HX_RETARGET: "main"}
 HX_REDIRECT: Final = "HX-Redirect"
 HX_REFRESH: Final = "HX-Refresh"
 HX_REFRESH_HEADER: Final = {HX_REFRESH: "true"}
@@ -8,3 +12,12 @@ HX_REFRESH_HEADER: Final = {HX_REFRESH: "true"}
 
 def HX_REDIRECT_HEADER(url: str):
     return {HX_REDIRECT: url}
+
+
+def is_boosted(request: Request) -> bool:
+    headers = request.headers
+
+    if "hx-boosted" in headers:
+        return headers["hx-boosted"] == "true"
+
+    return False
